42178343ade77b471a58480b511c909a7da451fa
John White <jaydub@MacBook-Pro-2.local>
2019-12-12 11:00:55 -0800

    left off on user component

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,9 +1,20 @@
 import React from "react"
+import styles from "./about-css-modules.module.css"
 import Container from "../components/container"
+console.log(styles)
+
+const User = props => (
+	<div className={styles.user}>
+		<img src={props.avatar} className={styles.avatar} alt=""/>
+	</div>
+)
 
 export default () => (
 	<Container>
 		<h1>About CSS Modules</h1>
 		<p>CSS Modules are cool</p>
+		<User
+			avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+		/>
 	</Container>
 )
