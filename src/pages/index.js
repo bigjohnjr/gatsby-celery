ed9e15e8ee4db8a8d3688b45a581ffea12e15292
John White <jaydub@MacBook-Pro-2.local>
2019-12-16 10:55:18 -0800

    typography plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 import Header from "../components/header";
 
 export default () => (
-    <div style={{ color: `purple` }}>
+    <div style={{ margin: `3rem auto`, maxWidth: 600, color: `purple` }}>
         <Link to="/contact/">Contact</Link>
         <Header headerText="Hello Gatsby!" />
         <h1>Celery Juicing</h1>
