79e07f8acad86f62f53ffba07c8459a2d1cd5809
John White <jaydub@MacBook-Pro-2.local>
2020-01-07 10:49:08 -0800

    graphiql learning

---
+++ src/pages/my-files.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <div>Hello world</div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          id
+          base
+          accessTime
+          modifiedTime
+          birthtime
+        }
+      }
+    }
+  }
+`
